d55d8a00d73ebeb7eea349504fd8ac9631cda8de
Rafael Mauricio <rafael_mauricio@hotmail.com>
2025-06-26 01:10:15 -0300

    feat: arquivos rivalix

---
+++ src/Components/TorneioParticipantes/TorneioParticipantes.jsx
@@ -0,0 +1,20 @@
+
+import style from "./TorneioParticipantes.module.css"
+import UserList from "../../Components/UserList/UserList"
+import torneios from "../../data/torneios"
+
+export default function TorneioParticipantes({ torneio }) {
+  const quantidade = torneio.totalVagas - torneio.vagasRestantes;
+
+  return (
+    <div className={style.container_participantes}>
+      <h2 className={style.titulo}>Participantes</h2>
+
+      <div className={style.lista_participantes}>
+        {Array.from({ length: quantidade }).map((_, i) => (
+          <UserList key={i} />
+        ))}
+      </div>
+    </div>
+  );
+}
